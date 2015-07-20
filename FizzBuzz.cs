3f2c287115037f2b15602dc938d7dbd010aa0b22
Ko Kumagai <ko.kumagai.ko@gmail.com>
2015-07-20 07:36:42 -0700

    add C#

---
+++ FizzBuzz.cs
@@ -0,0 +1,15 @@
+public class FizzBuzz {
+    public static void Main() {
+        for(int i = 1; i <= 100; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.Console.WriteLine("FizzBuzz");
+            } else if(i % 3 == 0) {
+                System.Console.WriteLine("Fizz");
+            } else if(i % 5 == 0) {
+                System.Console.WriteLine("Buzz");
+            } else {
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
