8106c97008520dcc2de5a68739e508704f9b876b
Ko Kumagai <ko.kumagai.ko@gmail.com>
2015-12-16 13:02:33 -0500

    add Hack

---
+++ fizzbuzz.hh
@@ -0,0 +1,12 @@
+<?hh
+for($i = 1; $i <= 100; $i++) {
+  if ($i % 15 == 0) {
+    print "FizzBuzz\n";
+  } elseif ($i % 3 == 0) {
+    print "Fizz\n";
+  } elseif ($i % 5 == 0) {
+    print "Buzz\n";
+  } else {
+    print $i."\n";
+  }
+}
